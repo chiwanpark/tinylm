62d33e0a5c4fc63c970dfb081f9f8e9796147c63
Chiwan Park <chiwanpark@hotmail.com>
2025-05-18 01:29:18 +0900

    chore: add core dependencies (#2)

--- ci/nvidia.py
+++ ci/nvidia.py
@@ -10,8 +10,13 @@ PYTHON_VERSION = "3.12"
 app = App(
     "ci_nvidia",
     image=Image.from_registry(f"nvidia/cuda:{CUDA_IMAGE_TAG}", add_python=PYTHON_VERSION)
+    .apt_install("python3-dev", "build-essential", "git", "libexpat1-dev")
     .pip_install("uv")
-    .add_local_dir(ROOT_PATH, remote_path=REMOTE_ROOT_PATH),
+    .add_local_dir(
+        ROOT_PATH,
+        remote_path=REMOTE_ROOT_PATH,
+        ignore=lambda p: p.is_relative_to(".venv"),  # ignore virtualenv
+    ),
 )
 
 
@@ -19,5 +24,10 @@ app = App(
 def do_test():
     import subprocess
 
-    subprocess.run("uv sync --group dev", check=True, shell=True, cwd=REMOTE_ROOT_PATH)
-    subprocess.run("TEST_NVIDIA=1 uv run poe test", check=True, shell=True, cwd=REMOTE_ROOT_PATH)
+    kwargs = {
+        "check": True,
+        "shell": True,
+        "cwd": REMOTE_ROOT_PATH,
+    }
+    subprocess.run("uv sync --group dev --compile-bytecode", **kwargs)
+    subprocess.run("TEST_NVIDIA=1 uv run --group dev poe test", **kwargs)
