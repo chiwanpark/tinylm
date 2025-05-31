9f160c95a1c72685c828b9fbd2b32e77d96212a2
Chiwan Park <chiwanpark@hotmail.com>
2025-06-01 01:04:28 +0900

    feat: implement fused GeluAndMul operation for GeGLU

---
+++ tinylm/layers/activation.py
@@ -0,0 +1,23 @@
+import torch
+from flashinfer.activation import gelu_and_mul, gelu_tanh_and_mul
+from torch.nn import functional as F
+
+from tinylm.layers.base import AcceleratedModule
+
+
+class GeluAndMul(AcceleratedModule[torch.Tensor]):
+    def __init__(self, approximate: str = "none") -> None:
+        super().__init__()
+        assert approximate in ("none", "tanh")
+        if approximate == "none":
+            self.op = gelu_and_mul
+        else:
+            self.op = gelu_tanh_and_mul
+        self.approximate = approximate
+
+    def forward_torch(self, x: torch.Tensor) -> torch.Tensor:
+        d = x.shape[-1] // 2
+        return F.gelu(x[..., :d], approximate=self.approximate) * x[..., d:]
+
+    def forward_flashinfer(self, x: torch.Tensor) -> torch.Tensor:
+        return self.op(x)
